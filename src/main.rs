413beb77052806cdddcd97b94639fba597f4cbd9
jesse <jesse.kruse@outlook.de>
2025-06-03 21:50:51 +0200

    numbering to subcomponents but commentet out, because it doesn't look nice

--- src/main.rs
+++ src/main.rs
@@ -101,7 +101,7 @@ impl LanguageServer for Backend {
 
 #[tokio::main]
 async fn main() {
-    let log_path = "/Users/jessekruse/Desktop/lsp_dbg_log.txt";
+    let log_path = "lsp_dbg_log.txt";
     let mut output = File::create(&log_path).unwrap();
     let line = "Created File";
     writeln!(output, "{}", line).unwrap();
@@ -125,6 +125,10 @@ fn get_inlay_hints(parse_tree: &Tree) -> Vec<InlayHint> {
     let mut inlay_hints: Vec<InlayHint> = Vec::new();
     let root_node = parse_tree.root_node();
 
+    // TODO: Just for Dbg
+    let log_path = "lsp_dbg_log.txt";
+    let mut f = File::options().append(true).open(&log_path).unwrap();
+
     for i in 0..root_node.child_count() {
         if let Some(segment) = root_node.child(i) {
             let mut field_count = 0;
@@ -135,25 +139,50 @@ fn get_inlay_hints(parse_tree: &Tree) -> Vec<InlayHint> {
                 if let Some(field) = segment.child(j) {
                     if field.kind() == "field_separator" {
                         field_count += 1;
-                        let hint_label = format!("{field_count}:");
+
+                        let mut hint_label = format!("{field_count}");
+                        if let Some(next_field) = segment.child(j + 1) {
+                            if next_field.kind() == "field" {
+                                hint_label = format!("{field_count}:");
+                            }
+                        }
                         inlay_hints.push(build_inlay_hint(hint_label, &field))
                     }
 
-                    if field.kind() == "field" {
-                        if let Some(repeat) = field.child(0) {
-                            let mut component_count = 1;
-                            for k in 0..repeat.child_count() {
-                                if let Some(component) = repeat.child(k) {
-                                    if component.kind() == "component_separator" {
-                                        component_count += 1;
-                                        let hint_label =
-                                            format!("{field_count}.{component_count}:");
-                                        inlay_hints.push(build_inlay_hint(hint_label, &component))
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    // if field.kind() == "field" {
+                    //     if let Some(repeat) = field.child(0) {
+                    //         let mut component_count = 1;
+                    //         for k in 0..repeat.child_count() {
+                    //             if let Some(component) = repeat.child(k) {
+                    //                 component_count += 1;
+                    //                 // if component.kind() == "component_separator" {
+                    //                 //     component_count += 1;
+                    //                 //     let hint_label =
+                    //                 //         format!("{field_count}.{component_count}:");
+                    //                 //     inlay_hints.push(build_inlay_hint(hint_label, &component))
+                    //                 // }
+
+                    //                 if component.kind() == "component" {
+                    //                     writeln!(&mut f, "in component path",).unwrap();
+
+                    //                     let mut sub_component_count = 1;
+                    //                     for l in 0..component.child_count() {
+                    //                         if let Some(sub_component) = component.child(l) {
+                    //                             sub_component_count += 1;
+                    //                             let hint_label = format!(
+                    //                                 "{field_count}.{component_count}.{sub_component_count}:"
+                    //                             );
+                    //                             inlay_hints.push(build_inlay_hint(
+                    //                                 hint_label,
+                    //                                 &sub_component,
+                    //                             ))
+                    //                         }
+                    //                     }
+                    //                 }
+                    //             }
+                    //         }
+                    //     }
+                    // }
                 }
             }
         }
@@ -166,11 +195,16 @@ fn build_inlay_hint(hint_label: String, child: &Node) -> InlayHint {
     let position = child.start_position();
     let row = position.row;
     let start = position.column;
+    let mut start_idx = start as u32;
+
+    if child.kind() == "field_separator" {
+        start_idx += 1
+    }
 
     InlayHint {
         position: Position {
             line: row as u32,
-            character: start as u32 + 1,
+            character: start_idx,
         },
         label: InlayHintLabel::String(hint_label),
         data: None,
